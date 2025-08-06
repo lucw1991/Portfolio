a974ce918ab91bae2ff864fe6c69fd8b4cf4776c
lucw1991 <105253783+lucw1991@users.noreply.github.com>
2025-08-06 11:11:00 -0400

    Update Main.java
    Established this branch's code as the raw starting point for my Leetcode solutions.

--- MergeSortedArray/src/Main.java
+++ MergeSortedArray/src/Main.java
@@ -1,24 +1,7 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
-import funcs.MergeSortedArray.MergeSortedArray;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        int[] nums1 = {1,2,3,0,0,0};
-        int m = 3;
-
-        int nums2[] = {2,5,6};
-        int n = 3;
-
-        // Create instance of Merge class
-        MergeSortedArray merger = new MergeSortedArray();
-        merger.merge(nums1, m, nums2, n);
-
-        // Added spacing just for my own preferences
-        System.out.printf("Hello, here is your merged array!\n" +  "     " + Arrays.toString(nums1));
+        System.out.printf("Hello, this is the main working file in my portfolio!");
     }
 
 }
